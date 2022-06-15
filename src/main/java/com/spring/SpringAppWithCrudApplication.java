e6cda0fe70020d19af19ca6e7288c2ae3c9f931b
ShaiksardharAhammedBasha <smabasha222@gmail.com>
2022-06-15 17:04:00 +0530

    adding new files

--- src/main/java/com/spring/SpringAppWithCrudApplication.java
+++ src/main/java/com/spring/SpringAppWithCrudApplication.java
@@ -5,14 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringAppWithCrudApplication {
-	
 	private static final Logger logger = LoggerFactory.getLogger(SpringAppWithCrudApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppWithCrudApplication.class, args);
 		logger.info("in main class");
 	}
-
 }
